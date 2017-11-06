c97dba207b68eb1517337603d80963ec0de06f0d
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-06 15:55:36 -0800

    added some code to customers index controller action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,10 @@ class CustomersController < ApplicationController
   # protect_from_forgery with: :null_session
 
   def index
+    customers = Customer.all
 
+    render(
+      json: customers.as_json
+    )
   end
 end
