0f8dd3a6726b0a1f54133629bf92680c1b552338
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-07 15:19:25 -0800

    SRB - Adds first pass at rental model tests for relationships and validations

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,9 @@
 class Rental < ApplicationRecord
+  # relationships
   belongs_to :movie
   belongs_to :customer
 
+  # validations
+  validates :due_date, presence: true
   # don't need to validate that rentals are unique, a customer could check out the same movie on numerouse occasions
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,15 +1,62 @@
 require "test_helper"
 
 describe Rental do
+  let(:rental_one){rentals(:one)}
+  let(:customer){customers(:one)}
+  let(:movie){movies(:Goonies)}
+  let(:date_one){"2018-11-11"}
+
   describe "relationships" do
     it "has a customer" do
-      rental1.must_respond_to :customer
-      rental1.customer.must_be_kind_of Customer
+      rental_one.must_respond_to :customer
+      rental_one.customer.must_be_kind_of Customer
     end
 
     it "has a movie" do
-      rental1.must_respond_to :movie
-      rental1.movie.must_be_kind_of Movie
+      rental_one.must_respond_to :movie
+      rental_one.movie.must_be_kind_of Movie
+    end
+
+    # TODO: fill in these tests
+    # it "allows one customer to have multiple movies" do
+    #
+    # end
+    #
+    # it "allows one movie to have many customers" do
+    #
+    # end
+  end
+
+  describe "validations" do
+    it "can be created" do
+      before = Rental.count
+      new_rental = Rental.new(movie: movie, customer: customer, due_date: date_one)
+
+      new_rental.must_be :valid?
+      new_rental.save
+
+      Rental.count.must_equal before + 1
+    end
+
+    it "it require a movie" do
+      rental = Rental.new(customer: customer , due_date: date_one)
+      result = rental.save
+      result.must_equal false
+      rental.errors.messages.must_include :movie
+    end
+
+    it "requires a customer" do
+      rental = Rental.new(movie: movie, due_date: date_one)
+      result = rental.save
+      result.must_equal false
+      rental.errors.messages.must_include :customer
+    end
+
+    it "requires a due_date" do
+      rental = Rental.new(movie: movie, customer: customer)
+      result = rental.save
+      result.must_equal false
+      rental.errors.messages.must_include :due_date
     end
   end
 end
