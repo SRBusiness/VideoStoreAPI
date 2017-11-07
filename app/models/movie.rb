30180e7c4597fa3067c83a357f39ee67971df456
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-06 16:43:12 -0800

    Updates movies model tests and added fixture data

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,6 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
-  validates :id, presence: true
   validates :title, presence: true
   validates :overview, presence: true
   validates :release_date, presence: true
