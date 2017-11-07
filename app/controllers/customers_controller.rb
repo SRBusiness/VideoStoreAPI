732ed33bc6f503ba938046743d5195ca8f92ce84
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-06 16:07:09 -0800

    added more yml data

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,7 +5,8 @@ class CustomersController < ApplicationController
     customers = Customer.all
 
     render(
-      json: customers.as_json
+      json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movie_checked_out_count]),
+      status: :ok
     )
   end
 end
