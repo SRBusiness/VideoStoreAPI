a99a7b1ae1027ffb9522da0d9954391d4216a469
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-06 16:50:25 -0800

    add controller action and tests for movies index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,10 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+
+    render(
+      json: movies.as_json(only: [:id, :title, :release_date]),
+      status: :ok
+    )
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "is a real working route" do
+    get movies_path
+    must_respond_with :success
+  end
+
+  it "returns json" do
+    get movies_path
+    response.header['Content-Type'].must_include 'json'
+  end
+
+  it "returns an Array" do
+    get movies_path
+
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it "returns all of the movies" do
+    get movies_path
+
+    body = JSON.parse(response.body)
+    body.length.must_equal Movie.count
+  end
+
+  it "returns movies with exactly the required fields" do
+    keys = %w(id release_date title)
+    get movies_path
+
+    body = JSON.parse(response.body)
+    body.each do |movie|
+      movie.keys.sort.must_equal keys
+    end
+  end
+
+  it "returns an empty array if there are no movies" do
+    Movie.destroy_all
+
+    get movies_path
+
+    must_respond_with :success
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+    body.must_be :empty?
+  end
 end
