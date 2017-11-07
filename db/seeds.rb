7bb99ee3fea2bab3ff3a7170e7bf14fe38fa3206
VirtualLindsey <tapircode@gmail.com>
2017-11-07 03:28:46 -0800

    fixing customer seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'date'
 JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
-  this_customer.Customer.new
+  this_customer = Customer.new
   this_customer.name = customer["name"]
   this_customer.registered_at = customer["registered_at"]
   this_customer.address = customer["address"]
@@ -8,19 +8,17 @@ JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
   this_customer.state = customer["state"]
   this_customer.postal_code = customer["postal_code"]
   this_customer.phone = customer["phone"]
+  this_customer.save
 end
 
-id = 1
+
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
   this_movie = Movie.new
   this_movie.title = movie["title"]
   this_movie.overview = movie["overview"]
   this_movie.release_date = movie["release_date"]
   this_movie.inventory = movie["inventory"]
-  this_movie.id = id
-    id+= 1
   this_movie.created_at = Date.today
   this_movie.updated_at = Date.today
-  puts this_movie
   this_movie.save
 end
