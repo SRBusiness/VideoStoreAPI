e78984f94717d7d79a125cd5a52351ace1c7e3a9
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-06 17:18:33 -0800

    Updated error handling for movies show when a movie isn't found by the proper id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,20 @@ class MoviesController < ApplicationController
       status: :ok
     )
   end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render(
+        json: movie.as_json(only: [:title, :overview, :release_date, :inventory]),
+        status: :ok
+      )
+    else
+      render(
+        json: {errors: {id: "Movie ID: #{params[:id]} not found"} },
+        status: :not_found
+      )
+    end
+  end
 end
